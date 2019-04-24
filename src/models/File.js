77cd55b3ae16d704e588ea6c8ac58515f1cd6333
root <root@srv8001.net07.biguhill.info>
2019-04-24 14:46:29 -0300

    Alterado urls para heroku

--- src/models/File.js
+++ src/models/File.js
@@ -19,9 +19,9 @@ const File = new mongoose.Schema(
 );
 
 File.virtual('url').get(function(){ 
-return `http://net07.biguhill.info:3333/files/${encodeURIComponent(this.path)}`;
+    const url = process.env.URL || 'http://net07.biguhill.info:3333'
 
-
+    return `${url}/files/${encodeURIComponent(this.path)}`;
 
 
 });
