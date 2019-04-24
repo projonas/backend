c3d0b05d4b01b9a4211bee67963f62f6c291123e
root <root@srv8001.net07.biguhill.info>
2019-04-24 14:38:22 -0300

    Nodejs backend-start

---
+++ src/models/File.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const File = new mongoose.Schema(
+    {
+       title: { 
+           type:String,
+           required: true
+       },
+       path: { 
+           type: String, 
+           required: true
+       },
+    },
+    {
+     timestamps: true, 
+     toObject: { virtuals: true },
+     toJSON: { virtuals: true }
+    }
+);
+
+File.virtual('url').get(function(){ 
+return `http://net07.biguhill.info:3333/files/${encodeURIComponent(this.path)}`;
+
+
+
+
+});
+
+module.exports = mongoose.model('File', File);
